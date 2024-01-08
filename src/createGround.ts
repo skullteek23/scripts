16f64fd71926d459358d097963f31044e27a0f40
skullteek23 <pgoel681@gmail.com>
2024-01-08 12:03:58 +0530

    adde changes

--- src/createGround.ts
+++ src/createGround.ts
@@ -1,7 +1,8 @@
 import { ContactInfo, FacilityStatus, Ground, GroundAdditionalInfo, GroundFacility, GroundStatus, convertObjectToFirestoreData, getRandomString } from '@ballzo-ui/core';
 import * as admin from 'firebase-admin';
 
-const serviceAccount = require("../secret-keys/football-platform-dev-firebase-adminsdk-zwhyn-2f830bef43.json");
+// const serviceAccount = require("../secret-keys/football-platform-dev-firebase-adminsdk-zwhyn-2f830bef43.json"); //dev
+const serviceAccount = require("../secret-keys/football-platform-production-firebase-adminsdk-ofoor-e09ce7fcb2.json"); // prod
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -10,27 +11,39 @@ admin.initializeApp({
 export const GROUND_ID = getRandomString(20);
 
 export const GROUND_DETAILS = {
-    name: 'Test Ground',
-    address: 'Plot No 12, Vaishali Extension, Ramprastha Greens, Vaishali, Ghaziabad, Uttar Pradesh',
-    imageUrl: 'https://www.shutterstock.com/image-photo/background-texture-large-public-local-260nw-1724385472.jpg',
-    mapUrl: 'https://maps.app.goo.gl/xybb8nMuXxbEq4fDA',
+    name: 'Gallant Play Arena',
+    address: 'Plot No 11, Sector 6, Naale Wali Gali, near Arogya Hospital, Vaishali Extension, Ramprastha Greens, Vaishali, Ghaziabad, Uttar Pradesh',
+    imageUrl: [
+        'https://firebasestorage.googleapis.com/v0/b/football-platform-production.appspot.com/o/grounds%2FGallant-vaishali%2FIMG_2835.jpg?alt=media&token=ce924b5c-f6ac-4107-96a0-f66f6e0e2d75',
+        'https://firebasestorage.googleapis.com/v0/b/football-platform-production.appspot.com/o/grounds%2FGallant-vaishali%2FIMG_2837.jpg?alt=media&token=b54cde71-69c5-480a-8686-6f99adc5e920',
+        'https://firebasestorage.googleapis.com/v0/b/football-platform-production.appspot.com/o/grounds%2FGallant-vaishali%2FIMG_2854.jpg?alt=media&token=6cbc5e4a-207e-4623-ab11-55239e02080d',
+    ],
+    mapUrl: 'https://maps.app.goo.gl/CX6Jx5ewB36ZyWbs6',
     city: 'Ghaziabad',
     state: 'Uttar Pradesh',
     pincode: 201012,
-    teamPrice: 149,
-    individualPrice: 149,
-    description: 'No Description',
-    rules: 'No Rules',
-    website: 'www.abc.com',
-    contactEmail: 'abc@gmail.com',
-    contactPhone: '9999999991',
-    contactName: 'Bro Insta'
+    teamPrice: 145,
+    individualPrice: 145,
+    description: 'Looking to play football? Discover world-class football pitches near you and get ready for an exhilarating game that will ignite your passion for the sport! Serve up some excitement as you step onto Gallant Play Arena and unleash your inner champion.',
+    rules: 'N/A',
+    website: 'www.gallantplay.com',
+    contactEmail: 'play@gallantsports.in',
+    contactPhone: '7979731898',
+    contactName: 'Neetish Ranjan'
 }
 
 export const FACILITY_DETAILS = [
     {
-        name: 'Astroturf',
+        name: 'Turf A',
         playerSize: 14,
+    },
+    {
+        name: 'Turf B',
+        playerSize: 14,
+    },
+    {
+        name: 'Rooftop 6v6',
+        playerSize: 12,
     },
 ]
 
@@ -39,7 +52,7 @@ async function createGround() {
     ground.id = getRandomString(20);
     ground.name = GROUND_DETAILS.name;
     ground.addressLine = GROUND_DETAILS.address;
-    ground.imgLinks = [GROUND_DETAILS.imageUrl];
+    ground.imgLinks = [...GROUND_DETAILS.imageUrl];
     ground.mapLink = GROUND_DETAILS.mapUrl;
     ground.city = ground.city;
     ground.state = GROUND_DETAILS.state;
